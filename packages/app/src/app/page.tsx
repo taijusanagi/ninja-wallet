e9dc4c735708d0a2f07688d1636fa3bf71d50fb1
Taiju Sanagi <taijusanagi@Taijus-MacBook-Pro.local>
2023-05-20 12:24:37 +0200

    feat: add home ui

--- packages/app/src/app/page.tsx
+++ packages/app/src/app/page.tsx
@@ -1,9 +1,42 @@
-import { SismoConnectButton } from "@/components/SismoConnectButton";
+"use client";
+
+import {
+  SismoConnectButton as _SismoConnectButton,
+  AuthType,
+  SismoConnectClientConfig,
+} from "@sismo-core/sismo-connect-react";
+
+import Image from "next/image";
+import { useState } from "react";
 
 export default function Home() {
+  const [responseBytes, setResponseBytes] = useState("");
+
+  const config: SismoConnectClientConfig = {
+    appId: "0x49c751f1595e8b3016096a7df01d2a96",
+  };
+
   return (
     <main>
-      <SismoConnectButton />
+      <div className="h-screen relative w-full flex flex-col items-center justify-center text-center text-white px-12">
+        <div className="animate-fadeInOut backdrop-blur-md">
+          <Image src="/ninja_transparent.png" alt="Ninja Wallet" width={240} height={240} className="object-cover" />
+        </div>
+        <h1 className="text-4xl font-bold">Ninja Wallet</h1>
+        <h2 className="mt-4 text-2xl text-lg">Stealth Account Abstraction Wallet with Sismo</h2>
+        <div className="mt-8">
+          {!responseBytes && (
+            <_SismoConnectButton
+              config={config}
+              auth={{ authType: AuthType.VAULT }}
+              signature={{ message: "Your message" }}
+              onResponseBytes={async (bytes: string) => {
+                setResponseBytes(bytes);
+              }}
+            />
+          )}
+        </div>
+      </div>
     </main>
   );
 }
--- packages/app/tailwind.config.js
+++ packages/app/tailwind.config.js
@@ -1,18 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      animation: {
+        fadeInOut: "fadeInOut 3s infinite",
       },
+      keyframes: {
+        fadeInOut: {
+          "0%": { opacity: 0, "backdrop-filter": "blur(5px)" },
+          "50%": { opacity: 1, "backdrop-filter": "blur(0)" },
+          "100%": { opacity: 0, "backdrop-filter": "blur(5px)" },
+        },
+      },
+      colors: {
+        "custom-blue-1": "#15203B",
+        "custom-blue-2": "#1B2B52",
+        "custom-blue-3": "#16203B",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-custom": "linear-gradient(to right, var(--tw-gradient-stops))",
+      }),
     },
   },
   plugins: [],
-}
+};
