da2f9ae61f2295c5bd42663bb8a8a24ec5ae3036
Taiju Sanagi <taijusanagi@Taijus-MacBook-Pro.local>
2023-05-21 10:32:50 +0200

    fix: add webpack in nextjs

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+const nodeExternals = require("webpack-node-externals");
+
 module.exports = {
   experimental: {
     externalDir: true,
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.externals = [nodeExternals()];
+    }
   },
 };
